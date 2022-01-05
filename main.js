7fdea3f7281d170582d4ec172513992db575013a
Lucian Petic <lucian.petic14@gmail.com>
2022-01-05 10:50:09 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+links = document.querySelectorAll('a')
+
+str = ''
+
+for(let i = 0; i < links.length; i++){
+	if(links[i].classList.contains('ytd-playlist-panel-video-renderer')){
+        str += '\n"' + links[i].href + '"'
+	}
+}
+
+console.log(str)
---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import glob
+from urllib.parse import urlparse, parse_qs
+
+path = '/Users/lulu/Music/Music/Media.localized/Music/"Unknown Artist"/"Unknown Album"/'
+command = 'youtube-dl -x --embed-thumbnail --audio-format mp3 {}'
+
+
+def get_last_created():
+    list_of_files = glob.glob(path.replace('"', '') + '*')
+    return max(list_of_files, key = os.path.getctime).split('/')[-1]
+
+
+def download(array, again):
+    error = []
+    for elm in array:
+        link = urlparse(elm)
+        link = link.scheme + '://' + link.netloc + link.path + '?v=' + parse_qs(link.query)['v'][0]
+        ok = os.system('youtube-dl -x --embed-thumbnail --audio-format mp3 {}'.format(link))
+        if ok == 0:
+            os.system('mv *.mp3 {}'.format(path))
+            os.system('open {}"{}"'.format(path, get_last_created()))
+        else:
+            print("Error")
+            error.append(link)
+    print(error)
+
+    if len(error) != 0 and again < 5:
+        download(error, again + 1)
+    else:
+        return
+
+
+if __name__ == '__main__':
+    download(sys.argv[1:], 0)
+
